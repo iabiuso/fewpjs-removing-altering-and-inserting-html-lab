1092e5763871ef62c6292c6e5a0ce064eddd1e08
iabiuso <ignacioabiuso@gmail.com>
2021-04-03 10:02:56 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+
+main.remove()
+
+const newHeader = document.createElement('h1');
+
+document.body.appendChild(newHeader);
+
+
+
+newHeader.id = "victory";
+
+// Make sure you create an <h1> with id 'victory' with a sweet message in it").
+//to.include("is the champion")
+
+newHeader.innerHTML = "Ignacio is the champion";
